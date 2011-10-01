898f8558af630608db08121c66b6494cfdeffb7c
arjun02001@gmail.com <arjun02001@gmail.com@b6b6963d-f149-1fd4-6a6e-7ff3b1d2161a>
2011-10-01 06:22:59 +0000

    initial

---
+++ MiniSAAS/MiniSAAS.Back/Classes/DataManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MiniSAAS.Back.Classes
+{
+    public class DataManager
+    {
+        public static DataTable GetData(string sql)
+        {
+            DataTable dt = new DataTable();
+            SqlConnection con = null;
+            SqlDataAdapter da = null;
+            try
+            {
+                con = new SqlConnection(Constants.CONNECTION_STRING);
+                con.Open();
+                da = new SqlDataAdapter(sql, con);
+                da.Fill(dt);
+                da.Dispose();
+                con.Close();
+                con.Dispose();
+            }
+            catch (Exception ex)
+            {
+                da.Dispose();
+                con.Close();
+                con.Dispose();
+                throw;
+            }
+            return dt;
+        }
+
+        public static int SetData(string sql)
+        {
+            SqlConnection con = null;
+            SqlCommand com = null;
+            try
+            {
+                con = new SqlConnection(Constants.CONNECTION_STRING);
+                con.Open();
+                com = new SqlCommand(sql, con);
+                return com.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                con.Close();
+                con.Dispose();
+                com.Dispose();
+                throw;
+            }
+        }
+    }
+}
---
+++ MiniSAAS/MiniSAAS.Back/DataService.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+using MiniSAAS.Back.Classes;
+
+namespace MiniSAAS.Back
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DataService" in code, svc and config file together.
+    public class DataService : IDataService
+    {
+        public double DoWork()
+        {
+            return 12;
+        }
+    }
+}
